50e2c0ba47144e0df5604216cc1ba900e3cb83c3
MuHan <128204479+Muhansrc@users.noreply.github.com>
2024-09-26 22:37:42 +0800

    Update main.py
    修复存在的逻辑bug

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from utils.tools import *
 from utils.logo import logo
 
+# 需要开启的线程数量
+thread_number = 10
+
+
+
+
 if __name__ == "__main__":
     logo()
-
+    
     print(Fore.GREEN + "扫描程序正在初始化~~~" + Fore.RESET)
     urls = init_scan()
     print(Fore.GREEN + "初始化运行完毕，即将开始扫描~~~" + Fore.RESET)
@@ -16,8 +22,10 @@ if __name__ == "__main__":
             url = url[:-1]
         if not url.startswith(("http://", "https://")):
             url = "http://" + url
-    
-        scan_base(url)
+        if check_url(url):
+            scan_base(url,thread_number)
+        else:
+            pass
     print(Fore.GREEN + "基础扫描模式运行结束，正在处理结果~~~" + Fore.RESET)
     print()
 
@@ -30,8 +38,10 @@ if __name__ == "__main__":
                 url = url[:-1]
             if not url.startswith(("http://", "https://")):
                 url = "http://" + url
-        
-            scan_api_url(url)
+            if check_url(url):
+                scan_api_url(url,thread_number)
+            else:
+                pass
         print(Fore.GREEN + "前缀API扫描模式运行结束，正在处理结果~~~" + Fore.RESET)
         print()
         
@@ -45,14 +55,12 @@ if __name__ == "__main__":
                 if success_url.endswith("heapdump"):
                     print(Fore.GREEN + "扫描到存在heapdump文件，即将进行下载~~~" + Fore.RESET)
                     download_heapdump(success_url)
-                else:
-                    print(Fore.GREEN + "扫描结束，请前往result路径查看~~~" + Fore.RESET)
+            print(Fore.GREEN + "扫描结束，请前往result路径查看~~~" + Fore.RESET)
     else:
         for success_url in success_urls:
             success_url = success_url.strip()
             if success_url.endswith("heapdump"):
                 print(Fore.GREEN + "扫描到存在heapdump文件，即将进行下载~~~" + Fore.RESET)
                 download_heapdump(success_url)
-            else:
-                print(Fore.GREEN + "扫描结束，请前往result路径查看~~~" + Fore.RESET)
+        print(Fore.GREEN + "扫描结束，请前往result路径查看~~~" + Fore.RESET)
 
